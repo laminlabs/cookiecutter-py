d70795092d27dbad80795207f1814b2dc20445f6
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-06 16:00:34 +0200

    Let initial version be 0.0.1 (#28)

--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -13,6 +13,6 @@ This is the complete API reference:
    ExampleClass
 """
 
-__version__ = "0.1a1"  # pre-release for initial release 0.1.0
+__version__ = "0.0.1"  # denote a pre-release for 0.1.0 with 0.1a1
 
 from ._core import ExampleClass, example_function  # noqa
