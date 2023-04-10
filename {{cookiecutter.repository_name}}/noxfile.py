feaf611b253d903c6ee41ab90d39f71b65605f27
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-10 23:27:17 +0200

    :construction_worker: Do not lint with multiple Python versions

--- {{cookiecutter.repository_name}}/noxfile.py
+++ {{cookiecutter.repository_name}}/noxfile.py
@@ -12,7 +12,7 @@ from laminci.nox import (
 nox.options.reuse_existing_virtualenvs = True
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
